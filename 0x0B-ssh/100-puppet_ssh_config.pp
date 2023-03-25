09bb63b422c407246aec2dca67ae0bdb83c76e63
njokujr <njokuvictory216@gmail.com>
2023-03-25 09:13:50 -0700

    tasks-done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# Client configuration file (w/ Puppet)
+# Puppet to set changes in a server
+file_line { 'disable password login':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
